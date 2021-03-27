d81a42202b962266e9eb0985817deea753f17101
Kedar Padki <Kp2702@gmail.com>
2021-03-27 18:28:47 -0400

    created a react app, setup firebase, imported material-ui

---
+++ blueprint-app/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyAcf_D0EltZ5YRx81oWz8kL-j-cMFtQP9I",
+    authDomain: "blueprintloginpage.firebaseapp.com",
+    projectId: "blueprintloginpage",
+    storageBucket: "blueprintloginpage.appspot.com",
+    messagingSenderId: "908170588527",
+    appId: "1:908170588527:web:bf100155f1a8b4eb87b89a",
+    measurementId: "G-GPWLQGTPWE"
+})

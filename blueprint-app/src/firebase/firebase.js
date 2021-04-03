8dfe761a90e90547487e160ae63ecfee1900d208
Kedar Padki <Kp2702@gmail.com>
2021-04-03 16:01:11 -0400

    copied things from Kanish's test app to main blueprint app
    copied the files that did not exist in the blueprint app, but did not modify any existing files

--- blueprint-app/src/firebase/firebase.js
+++ blueprint-app/src/firebase/firebase.js
@@ -1,7 +1,8 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore'
 
-const app = firebase.initializeApp({
+const firebaseConfig = {
     apiKey: "AIzaSyAcf_D0EltZ5YRx81oWz8kL-j-cMFtQP9I",
     authDomain: "blueprintloginpage.firebaseapp.com",
     projectId: "blueprintloginpage",
@@ -9,4 +10,8 @@ const app = firebase.initializeApp({
     messagingSenderId: "908170588527",
     appId: "1:908170588527:web:bf100155f1a8b4eb87b89a",
     measurementId: "G-GPWLQGTPWE"
-})
+}
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;

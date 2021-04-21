09efcaad626caafcfb9059f556dc35fc8a6d3e0d
Kedar Padki <Kp2702@gmail.com>
2021-04-21 19:49:17 -0400

    fixes from merging

--- blueprint-app/src/firebase/firebase.js
+++ blueprint-app/src/firebase/firebase.js
@@ -10,14 +10,8 @@ const firebaseConfig = {
     messagingSenderId: "908170588527",
     appId: "1:908170588527:web:bf100155f1a8b4eb87b89a",
     measurementId: "G-GPWLQGTPWE"
-<<<<<<< HEAD:blueprint-app/src/firebase.js
-})
-//export const myFirebase = firebase.initializeApp(firebaseConfig);
-const baseDb = app.firestore();
-=======
 }
 
 export const myFirebase = firebase.initializeApp(firebaseConfig);
 const baseDb = myFirebase.firestore();
->>>>>>> 8dfe761a90e90547487e160ae63ecfee1900d208:blueprint-app/src/firebase/firebase.js
 export const db = baseDb;

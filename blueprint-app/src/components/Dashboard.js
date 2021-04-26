37423ab870531e24bcdfb9a8a82a9f3d073959c2
Kedar Padki <Kp2702@gmail.com>
2021-04-26 17:49:56 -0400

    Cleaned up some code (all functionality exactly the same)

--- blueprint-app/src/components/Dashboard.js
+++ blueprint-app/src/components/Dashboard.js
@@ -1,8 +1,13 @@
-import { Avatar, Card, Grid, IconButton, Menu, MenuItem, MenuList, TextField } from "@material-ui/core";
-import SearchIcon from '@material-ui/icons/Search';
-import StarIcon from '@material-ui/icons/Star';
-import StarOutlineIcon from '@material-ui/icons/StarOutline';
-import StarHalfIcon from '@material-ui/icons/StarHalf';
+import {
+  Avatar,
+  Card,
+  Grid,
+  IconButton,
+  Menu,
+  MenuItem,
+  TextField,
+} from "@material-ui/core";
+import { Search, Star, StarOutline, StarHalf } from "@material-ui/icons";
 import { Component } from "react";
 import { connect } from "react-redux";
 import { logoutUser } from "../actions";
@@ -13,88 +18,91 @@ class Dashboard extends Component {
     dispatch(logoutUser());
   };
 
-  state = { anchorEl: null }
+  state = { anchorEl: null };
 
   handleOpenMenu = (e) => {
-    this.setState({ anchorEl: e.currentTarget })
-  }
+    this.setState({ anchorEl: e.currentTarget });
+  };
 
   handleCloseMenu = () => {
-    this.setState({ anchorEl: null })
-  }
+    this.setState({ anchorEl: null });
+  };
 
   render() {
     const { isLoggingOut, logoutError } = this.props;
     return (
       <div>
-        <IconButton onClick = {this.handleOpenMenu} aria-controls='menu'>
-          <Avatar/>
-        </IconButton>
-
-        <Menu 
-          id='menu' 
-          onClose={this.handleCloseMenu} 
-          anchorEl={this.state.anchorEl} 
-          open={Boolean(this.state.anchorEl)}
-        >
-          <MenuItem onClick={this.handleLogout}>Logout</MenuItem>
-        </Menu>
+        <header>
+          <IconButton onClick={this.handleOpenMenu} aria-controls='menu'>
+            <Avatar />
+          </IconButton>
+          <Menu
+            id='menu'
+            onClose={this.handleCloseMenu}
+            anchorEl={this.state.anchorEl}
+            open={Boolean(this.state.anchorEl)}
+          >
+            <MenuItem onClick={this.handleLogout}>Logout</MenuItem>
+          </Menu>
+        </header>
 
         <Grid
           container
-          direction="column"
-          alignItems="center"
+          direction='column'
+          alignItems='center'
           spacing={0}
-          style={{ minHeight: '100vh' }}
+          style={{ minHeight: "100vh" }}
         >
-
           <h1>Classes</h1>
-          <Card style={{marginBottom: '15px'}}>
-            <TextField 
-              style={{minWidth: '50vw'}} 
-            />
-            <SearchIcon style={{marginTop: '4px'}} />
+          <Card style={{ marginBottom: "15px" }}>
+            <TextField style={{ minWidth: "50vw" }} />
+            <Search style={{ marginTop: "4px" }} />
           </Card>
 
-          <Card style={{backgroundColor: '#D3D3D3', marginBottom: '15px', marginRight: '5px', marginLeft: '5px', padding:'15px'}}>
-            <Grid
-              container
-              alignItems="center"
-              justify="center"
-            >
+          <Card
+            style={{
+              backgroundColor: "#D3D3D3",
+              marginBottom: "15px",
+              marginRight: "5px",
+              marginLeft: "5px",
+              padding: "15px",
+            }}
+          >
+            <Grid container alignItems='center' justify='center'>
               <h2>Class 1</h2>
-              <StarIcon style={{ marginLeft: '100px' }}/>
-              <StarIcon/>
-              <StarIcon/>
-              <StarOutlineIcon />
-              <StarOutlineIcon />
-              <p style={{ marginLeft:'10px' }}>(191 reviews)</p>
+              <Star style={{ marginLeft: "100px" }} />
+              <Star />
+              <Star />
+              <StarOutline />
+              <StarOutline />
+              <p style={{ marginLeft: "10px" }}>(191 reviews)</p>
             </Grid>
           </Card>
 
-          <Card style={{backgroundColor: '#D3D3D3', marginBottom: '15px', marginRight: '5px', marginLeft: '5px', padding:'15px'}}>
-            <Grid
-              container
-              alignItems="center"
-              justify="center"
-            >
+          <Card
+            style={{
+              backgroundColor: "#D3D3D3",
+              marginBottom: "15px",
+              marginRight: "5px",
+              marginLeft: "5px",
+              padding: "15px",
+            }}
+          >
+            <Grid container alignItems='center' justify='center'>
               <h2>Class 2</h2>
-              <StarIcon style={{ marginLeft: '100px' }}/>
-              <StarIcon/>
-              <StarIcon/>
-              <StarHalfIcon />
-              <StarOutlineIcon />
-              <p style={{ marginLeft:'10px' }}>(211 reviews)</p>
+              <Star style={{ marginLeft: "100px" }} />
+              <Star />
+              <Star />
+              <StarHalf />
+              <StarOutline />
+              <p style={{ marginLeft: "10px" }}>(211 reviews)</p>
             </Grid>
           </Card>
-
-          
-          {isLoggingOut && <p>Logging Out....</p>}
-          {logoutError && <p>Error logging out</p>}
-        
         </Grid>
-      </div>
 
+        {isLoggingOut && <p>Logging Out....</p>}
+        {logoutError && <p>Error logging out</p>}
+      </div>
     );
   }
 }
@@ -102,7 +110,7 @@ class Dashboard extends Component {
 function mapStateToProps(state) {
   return {
     isLoggingOut: state.auth.isLoggingOut,
-    logoutError: state.auth.logoutError
+    logoutError: state.auth.logoutError,
   };
 }
 

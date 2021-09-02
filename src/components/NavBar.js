d50cbf16cf7dc6e16fedfa6a2ef98ad9772cb435
Kedar Padki <Kp2702@gmail.com>
2021-09-02 01:20:12 -0400

    update to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -41,8 +41,7 @@ const NavBar = ({
     };
   });
 
-  if (backToLanding && !fromLandingPage)
-    return <Redirect to="/BlueprintIncubatorGeneral" />;
+  if (backToLanding && !fromLandingPage) return <Redirect to="/" />;
 
   return (
     <Grid container style={{ backgroundColor: backgroundColor }}>
